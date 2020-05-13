af1bb5e89ab718200ac3b5d3e9cb91e158cc1a87
MrRisoni <vanguelisk@gmail.com>
2020-05-13 20:48:25 +0300

    fetch zfs list

---
+++ api.js
@@ -0,0 +1,68 @@
+require('custom-env').env('staging')
+const mongoose = require('mongoose');
+const schemas = require('./mongoSchemas');
+const port = process.env.PORT || 3500;
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const connStr = 'mongodb+srv://' + process.env.MONGO_USR + ':' + process.env.MONGO_PASSWD + '@' + process.env.MONGO_HOST + '/' + process.env.MONGO_DBNAME + '?retryWrites=true&w=majority';
+mongoose.connect(connStr, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.get('/api/zfslist', (req,res) => {
+    // get host name
+    var query  = schemas.zfsMdl.where({ hostId: '5ebc2a530d0ffd1c6e5e8a05' });
+    query.findOne(function (err, zfslistdata) {
+         if (err)  {
+             console.log(err);
+        res.sendStatus(500);
+         }
+        if (zfslistdata) {
+            // doc may be null if no document matched
+            res.send(zfslistdata);
+        }
+    })
+
+
+
+});
+http.listen(port, (req, res) => {
+    console.log('Server listening on port number', port);
+});
+
+
+/*
+var zfsMdlItm = mongoose.model('ZFSListItem', schemas.zfsListItemSchema);
+var zfsMdl = mongoose.model('ZFSList', schemas.zfsListSchema);
+
+
+var hjem = new zfsMdlItm({
+    name: 'zpisina/usr/home',
+    usedRaw: '180K',
+    usedMib: 0.18,
+    availRaw: '31.8G',
+    availMib: 31800,
+    mounted: '/usr/home'
+});
+
+var root = new zfsMdlItm({
+    name: 'zpisina',
+    usedRaw: '1.72G',
+    usedMib: 1720,
+    used: 0.054,
+    availRaw: '31.8G',
+    availMib: 31800,
+    mounted: '/zpisina'
+});
+
+var freeBsd = new zfsMdl({hostId: '5ebc2a530d0ffd1c6e5e8a05', items: [root,hjem]});
+freeBsd.save().then(() => console.log('freeBsd')).catch(err => console.log(err));
+
+console.log('OK');
+
+*/
--- mongoSchemas.js
+++ mongoSchemas.js
@@ -1,13 +1,37 @@
 const mongoose = require('mongoose');
 
 var hostSchema = new mongoose.Schema({
-    hostname     : { type : String , unique : true, required : true },
-    description : { type:  String  },
-    fileSys     : { type : String , required : true },
+    hostname: {type: String, unique: true, required: true},
+    description: {type: String},
+    fileSys: {type: String, required: true},
 });
 
 
+var zfsListItemSchema = new mongoose.Schema({
+    name: String,
+    usedRaw: String,
+    usedMib: Number,
+    used:{type: Number, default: 0.000001},
+    availRaw: String,
+    availMib: Number,
+    mounted: String
+});
+
+
+var zfsListSchema = new mongoose.Schema({
+    hostId: {type: String, unique: true, required: true},
+    stamp: {type: Date, default: Date.now},
+    items: [zfsListItemSchema],
+});
+
+
+var zfsMdl = mongoose.model('ZFSList', zfsListSchema);
+
+
 module.exports = {
-    hostSchema
+    hostSchema,
+    zfsListSchema,
+    zfsListItemSchema,
+    zfsMdl
 }
 
